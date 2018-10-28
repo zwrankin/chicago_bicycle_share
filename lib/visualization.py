b5003155657357bfc733c029c23a36804e37aac8
Zane Rankin <zrankin@ihme.washington.edu>
2018-10-28 15:39:50 -0500

    add map_stations function

--- lib/visualization.py
+++ lib/visualization.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
+import folium
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, scoring=None,
                         n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
@@ -31,3 +32,34 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, scoring=None
 
     plt.legend(loc="best")
     return plt
+
+
+def map_stations(df, indicator: str):
+    """
+    Creates Folium map of chosen indicator with circle size corresponding to indicator value.
+    :param df: pd.DataFrame with  Required columns: ['latitude', 'longitude', 'color', {indicator}]
+    :param indicator: column name of value to plot
+    :return:
+    """
+
+    REQUIRED_COLS = ['latitude', 'longitude', 'color', indicator]
+    assert(all([col in df.columns for col in REQUIRED_COLS])), f'df is missing one or more of {REQUIRED_COLS}'
+
+    m = folium.Map(location=[41.9, -87.6], zoom_start=11)
+    for i in df.index:
+        folium.Circle((df['latitude'][i], df['longitude'][i]), radius=str(df[indicator][i] * 20),
+                      color=df['color'][i]).add_to(m)
+
+    # Folium doesn't have good legend support, HMTL adapted from
+    # https://medium.com/@bobhaffner/creating-a-legend-for-a-folium-map-c1e0ffc34373
+    legend_html = '''
+                    <div style="position: fixed; 
+                                bottom: 500px; left: 500px; width: 100px; height: 90px; 
+                                border:2px solid grey; z-index:9999; font-size:14px;
+                                ">&nbsp; Year Built <br>
+                                  &nbsp; 2015 &nbsp; <i class="fa fa-map-marker fa-2x" style="color:red"></i><br>
+                                  &nbsp; <2015 &nbsp; <i class="fa fa-map-marker fa-2x" style="color:blue"></i>
+                    </div>
+                    '''
+    m.get_root().html.add_child(folium.Element(legend_html))
+    return m
